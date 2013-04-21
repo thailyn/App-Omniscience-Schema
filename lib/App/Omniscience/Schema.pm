150d25d13c27cadb2826cdf6da70cebb022f5494
Charles Macanka <cmacanka@gmail.com>
2013-04-21 19:53:04 -0400

    Added version number to Schema.pm.

--- lib/App/Omniscience/Schema.pm
+++ lib/App/Omniscience/Schema.pm
@@ -13,7 +13,8 @@ __PACKAGE__->load_namespaces;
 
 # Created by DBIx::Class::Schema::Loader v0.07035 @ 2013-04-21 19:46:15
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:zleO/i9tUqXUP8l5TIQPAQ
-
+our $VERSION = '0.001';
+$VERSION = eval $VERSION;
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
 __PACKAGE__->meta->make_immutable(inline_constructor => 0);
