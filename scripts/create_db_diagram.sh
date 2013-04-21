fd68cf7bb3e0dd650cc8189d259ab0a86291fdfb
Charles Macanka <cmacanka@gmail.com>
2013-04-21 16:15:38 -0400

    Added script to create a diagram of the database.

---
+++ scripts/create_db_diagram.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Remove the backup files.
+rm -f schema/schema.yaml.prev
+rm -f schema/schema.png.prev
+# Rename the current schema files to act as a backup.
+mv schema/schema.yaml schema/schema.yaml.prev
+mv schema/schema.png schema/schema.png.prev
+
+# Create the YAML dump of the db
+sqlt --from DBI --dsn dbi:Pg:dbname=omniscience --db-user omniscience_user \
+  --to YAML > schema/schema.yaml
+# Create a diagram from the YAML file.
+sqlt-graph --from=YAML --color --show-datatypes --show-sizes \
+  --show-constraints --output-type png schema/schema.yaml > schema/schema.png
